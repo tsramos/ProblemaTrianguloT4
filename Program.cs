56a42bbecc765e3b13da61a32659a145f79bc220
Thiago Ramos <thiago.s.ramos@outlook.com>
2024-03-08 19:52:41 -0300

    Resolvido problema triangulo com logica tradicional

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace ProblemaTriangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Informe os lados do triângulo X;");
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inform os lados do triângulo Y");
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+            double p = (xA + xB + xC) / 2;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            p = (yA + yB + yC) / 2;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine($"Valor area X : {areaX.ToString("F2")}");
+            Console.WriteLine($"Valor area Y : {areaY.ToString("F2")}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Area de X é maior");
+            }
+            else 
+            {
+                Console.WriteLine("Area de Y é maior");
+            }
         }
     }
 }
